79123e02e14a67340483357637a0902ec51aecdc
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-06 08:52:00 +0500

    final commit

---
+++ Star Rating/script.js
@@ -0,0 +1,37 @@
+const stars = document.querySelectorAll(".fa-solid");
+const rating = document.querySelector(".rating");
+let currentTotalSelectedStars = -1;
+
+const handleUpdateRatingState = (getCurrentRatingValue) => {
+  for (let i = 0; i < 5; i++) {
+    if (i < getCurrentRatingValue) {
+      stars[i].classList.replace("fa-solid", "fa");
+    } else {
+      stars[i].classList.replace("fa", "fa-solid");
+    }
+  }
+};
+const handleMouseOver = (event) => {
+  const currentRatingValue = event.target.dataset.rating;
+  console.log(currentRatingValue);
+  if (!currentRatingValue) return;
+  else {
+    handleUpdateRatingState(currentRatingValue);
+  }
+};
+const handleOnClick = (event) => {
+  let currentRatingValue = event.target.dataset.rating;
+  currentTotalSelectedStars = currentRatingValue;
+  handleUpdateRatingState(currentTotalSelectedStars);
+  rating.textContent = currentTotalSelectedStars;
+};
+const handleMouseLeave = () => {
+  handleUpdateRatingState(currentTotalSelectedStars);
+};
+
+stars.forEach((starItem, index) => {
+  starItem.dataset.rating = index + 1;
+  starItem.addEventListener("mouseover", handleMouseOver);
+  starItem.addEventListener("click", handleOnClick);
+  starItem.addEventListener("mouseleave", handleMouseLeave);
+});
