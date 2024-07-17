33cbcd121b080dc4e4bf13ea2c3ad5ab0fbb7853
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-17 12:37:30 +0500

    added editbtn functionality

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,23 +1,19 @@
 const addBtn = document.querySelector(".add-btn");
 const inputField = document.querySelector(".input");
-const todoListContainer = document.querySelector(".todo-lists");
-
 const ulTodo = document.querySelector(".ul-todo");
-const todos = [];
+let todos = [];
+let editTodoId = null;
 
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (inputField.value.trim() !== "") {
-    let task = { id: Date.now(), inputfield: inputField.value };
-    todos.unshift(task);
-    console.log(todos);
-    displayTodos();
-    inputField.value = "";
-  } else {
-    alert("Please add task here....");
-  }
-});
+const saveTodosToLocalStorage = () => {
+  window.localStorage.setItem("todoList", JSON.stringify(todos));
+};
 
+const loadTodosFromLocalStorage = () => {
+  const todoItems = JSON.parse(window.localStorage.getItem("todoList"));
+  return todoItems ? todoItems : [];
+};
+
+// Display todos
 const displayTodos = () => {
   ulTodo.innerHTML = "";
   if (todos.length > 0) {
@@ -29,6 +25,7 @@ const displayTodos = () => {
       let checkBoxInput = document.createElement("input");
       let deleteBtn = document.createElement("button");
       let editBtn = document.createElement("button");
+
       deleteBtn.setAttribute("id", "dlt-btn-" + todo.id);
       deleteBtn.setAttribute("class", `${todo.id}`);
       editBtn.classList.add("edit-btn");
@@ -56,6 +53,7 @@ const displayTodos = () => {
           (todoItem) => todoItem.id === deleteBtnClass
         );
         todos.splice(updateTodo, 1);
+        saveTodosToLocalStorage();
         displayTodos();
       });
 
@@ -65,21 +63,45 @@ const displayTodos = () => {
           editBtn.style.cursor = "not-allowed";
           todoSpan.style.textDecoration = "line-through";
         } else {
-          editBtn.setAttribute("disabled", "false");
+          editBtn.removeAttribute("disabled");
           editBtn.style.cursor = "pointer";
-          todoSpan.style.textDecoration = "";
+          todoSpan.style.textDecoration = "none";
         }
       });
+
+      editBtn.addEventListener("click", (e) => {
+        inputField.value = todo.inputfield;
+        editTodoId = todo.id;
+        addBtn.innerText = "Update Task";
+      });
     });
-  }
-  {
-    (() => {
-      if (todos.length === 0) {
-        let h3 = document.createElement("h3");
-        h3.innerText = "No list available";
-        h3.style.color = "#850000";
-        ulTodo.append(h3);
-      }
-    })();
+  } else {
+    let h3 = document.createElement("h3");
+    h3.innerText = "No list available";
+    h3.style.color = "#850000";
+    ulTodo.append(h3);
   }
 };
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputField.value.trim() !== "") {
+    if (editTodoId) {
+      const editTodoIndex = todos.findIndex((todo) => todo.id === editTodoId);
+      todos[editTodoIndex].inputfield = inputField.value;
+      editTodoId = null;
+      addBtn.innerText = "Add Task";
+    } else {
+      let task = { id: Date.now(), inputfield: inputField.value };
+      todos.unshift(task);
+    }
+    saveTodosToLocalStorage();
+    displayTodos();
+    inputField.value = "";
+  } else {
+    alert("Please add task here....");
+  }
+});
+
+todos = loadTodosFromLocalStorage();
+displayTodos();
